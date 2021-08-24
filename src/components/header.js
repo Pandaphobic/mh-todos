8bac4e46309a947ae8ff6a465a074b93d9fef2d2
Chris Steffes <c.steffes@live.ca>
2021-08-24 08:14:22 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <Typography>MH Todo List</Typography>
+    </div>
+  );
+}
---
+++ src/components/todo-list.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FolderIcon from "@material-ui/icons/Folder";
+import {
+  Container,
+  List,
+  Typography,
+  ListItemIcon,
+  ListItemText,
+  ListItem,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+  Typography: {
+    fontFamily: ["Rajdhani"],
+  },
+}));
+
+function generate(element) {
+  return [0, 1, 2].map(value =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+}
+
+export default function TodoList() {
+  const classes = useStyles();
+  // User for rendering secondary text
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h6" className={classes.title}>
+        Todo List
+      </Typography>
+      <div className={classes.demo}>
+        <List>
+          {generate(
+            <ListItem>
+              <ListItemIcon>
+                <FolderIcon />
+              </ListItemIcon>
+              <ListItemText primary="Title Text" secondary={"Body Text"} />
+            </ListItem>
+          )}
+        </List>
+      </div>
+    </Container>
+  );
+}
