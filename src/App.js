efb015b35564b7d30e780019484d467012f2eeeb
Chris Steffes <c.steffes@live.ca>
2021-08-27 23:36:19 -0500

    IT WORKS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import { useState } from "react";
-import Todos from "./components/todos";
-import Header from "./components/header";
-import { Container, List } from "@material-ui/core";
+import "./App.css"
+import { useState, useEffect } from "react"
+import Todos from "./components/todos"
+import Header from "./components/header"
+import { Container, List } from "@material-ui/core"
 
 function App() {
   // App Level State for Todo items
@@ -10,42 +10,38 @@ function App() {
     {
       id: 0,
       title: "something",
-      description: "something a bit longer",
+      description: "something a bit longer"
     },
     {
       id: 1,
       title: "MORE",
-      description: "More about this topic",
+      description: "More about this topic"
     },
     {
       id: 2,
       title: "It's about time",
-      description: "Maybe we should discuss the weather",
-    },
-  ]);
+      description: "Maybe we should discuss the weather"
+    }
+  ])
 
   const deleteTodo = id => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
 
   const addTodo = todo => {
-    console.log(todo);
-  };
+    console.log(todos)
+    setTodos([todo, ...todos])
+    console.log(todos)
+  }
 
   return (
     <div className="App">
       <Container maxWidth="sm">
         <Header onAddTodo={addTodo} />
-        <List>
-          {todos.length > 0 ? (
-            <Todos todos={todos} onDelete={deleteTodo} />
-          ) : (
-            "No Todos Set"
-          )}
-        </List>
+        <List>{todos.length > 0 ? <Todos todos={todos} onDelete={deleteTodo} /> : "No Todos Set"}</List>
       </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,21 @@
-import React from "react";
-import { Typography, Grid } from "@material-ui/core";
-import TodoDialog from "./todoModal";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react"
+import { Typography, Grid } from "@material-ui/core"
+import TodoDialog from "./todoModal"
+import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
   title: {
     margin: theme.spacing(4, 0, 2),
     fontFamily: "Rajdhani",
-    fontWeight: 600,
-  },
-}));
-export default function Header(onAddTodo) {
-  const classes = useStyles();
+    fontWeight: 600
+  }
+}))
+export default function Header({ onAddTodo }) {
+  const classes = useStyles()
 
   return (
     <>
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-      >
+      <Grid container spacing={3} direction="row" alignItems="center" justifyContent="center">
         <Grid align="left" item xs={9}>
           <Typography variant="h5" className={classes.title}>
             Todo List
@@ -30,10 +24,10 @@ export default function Header(onAddTodo) {
 
         <Grid align="right" item xs={3}>
           <Typography variant="h6" className={classes.title}>
-            <TodoDialog onAddTodo />
+            <TodoDialog onAddTodo={onAddTodo} />
           </Typography>
         </Grid>
       </Grid>
     </>
-  );
+  )
 }
